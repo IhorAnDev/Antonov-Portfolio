80a5b5215381b38cbb4812d2f537b4489c5f4e25
IgorAnDev <gosha.8686@gmail.com>
2023-04-01 19:41:14 +0300

    build

---
+++ build/precache-manifest.7001516533fcaeea15d8cca096ea092d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c54074931dc29775d855894ebf492a7b",
+    "url": "/Antonov-Portfolio/index.html"
+  },
+  {
+    "revision": "91bca7596740ebd9bd99",
+    "url": "/Antonov-Portfolio/static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "e93d162c3d92b3ea59e2",
+    "url": "/Antonov-Portfolio/static/css/main.4a55ff9b.chunk.css"
+  },
+  {
+    "revision": "91bca7596740ebd9bd99",
+    "url": "/Antonov-Portfolio/static/js/2.94cdc7ec.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Antonov-Portfolio/static/js/2.94cdc7ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e93d162c3d92b3ea59e2",
+    "url": "/Antonov-Portfolio/static/js/main.640be186.chunk.js"
+  },
+  {
+    "revision": "52ef86ba4419c5f59812",
+    "url": "/Antonov-Portfolio/static/js/runtime-main.11c5e0d7.js"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/Antonov-Portfolio/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/Antonov-Portfolio/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Antonov-Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "964db67682f82077ab44ebfbd9796cf6",
+    "url": "/Antonov-Portfolio/static/media/avatar.964db676.png"
+  },
+  {
+    "revision": "44df6696421d42cbccd83fa71c3b790e",
+    "url": "/Antonov-Portfolio/static/media/bg.44df6696.jpg"
+  },
+  {
+    "revision": "b2124bd222a940f18099649c47ca1771",
+    "url": "/Antonov-Portfolio/static/media/loading.b2124bd2.svg"
+  },
+  {
+    "revision": "8b31d52f1299e2c661345526f7dd0a15",
+    "url": "/Antonov-Portfolio/static/media/qr.8b31d52f.png"
+  }
+]);
